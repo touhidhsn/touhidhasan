17ef59df667a9965c229498b287dcada1b0c5a9e
Touhid Hasan <touhidhsn@gmail.com>
2024-01-28 15:52:04 +0600

    colormode func addded

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,46 @@
-import { ThemeProvider, Typography, createTheme } from '@mui/material';
+import {
+  CssBaseline,
+  Switch,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from '@mui/material';
 import './App.css';
-import Button from '@mui/material/Button';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: '"Fira Code", cursive',
-  },
-});
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 function App() {
+  const [colorMode, setColorMode] = useState(false);
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: '"Fira Code", cursive',
+    },
+    palette: {
+      mode: colorMode ? 'dark' : 'light',
+    },
+  });
+
+  const handleChange = () => {
+    colorMode === true ? setColorMode(false) : setColorMode(true);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <Typography>
-        <h1>Hello World</h1>
-        <Button>Click Here Honey</Button>
-      </Typography>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Typography>
+          <CssBaseline />
+          <h1>Hello World</h1>
+          <Button>Click Here Honey</Button>
+          <Switch
+            checked={colorMode}
+            onChange={handleChange}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+        </Typography>
+      </ThemeProvider>
+    </>
   );
 }
 
