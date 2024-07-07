cdab2e05f6d6584c8fd1142837312cfcaa88d492
Touhid Hasan <touhidhsn@gmail.com>
2024-07-07 11:36:57 +0600

    nav bar created

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9e53fe361bf1c49d")
+/******/ 		__webpack_require__.h = () => ("5dbfc5dcb686d679")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/components/ContainerBlock.js
+++ app/components/ContainerBlock.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import React from 'react';
-import Navbar from './navbar';
+import Navbar from './Navbar';
 
 const ContainerBlock = ({ children }) => {
   const meta = {
