5184645fe0d42933fe1ce87057c54383c8de253e
Md Touhid Hasan <104325388+touhidhsn@users.noreply.github.com>
2024-09-21 21:27:44 +0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import ContainerBlock from '@/app/components/ContainerBlock';
 import FavouriteProjects from '@/app/components/FavoriteProjects';
 import Hero from '@/app/components/Hero';
+import LatestCode from '@/app/components/LatestCode';
 import React from 'react';
 
 const Home = () => {
