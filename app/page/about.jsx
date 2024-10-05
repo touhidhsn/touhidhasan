6664bafbc19e16d69fd155ea2a99c9f6311ba431
Touhid Hasan <touhidhsn@gmail.com>
2024-10-05 22:04:12 +0600

    contact page and component created

--- app/page/about.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ContainerBlock from '../components/ContainerBlock';
-
-const about = () => {
-  return <ContainerBlock>About</ContainerBlock>;
-};
-
-export default about;
---
+++ pages/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContainerBlock from '../app/components/ContainerBlock';
+import Contact from '@/app/components/Contact';
+
+const contact = () => {
+  return (
+    <ContainerBlock>
+      <Contact />
+    </ContainerBlock>
+  );
+};
+
+export default contact;
