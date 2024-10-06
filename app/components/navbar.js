cd495be935fa455d96d8e5a89755da3071026dbc
Touhid Hasan <touhidhsn@gmail.com>
2024-10-06 13:26:51 +0600

    some refactor made

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,16 +1,14 @@
 'use client';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
 import userData from '../constants/data';
+import NavLinks from './NavLinks.jsx';
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
   const [mounted, setmounted] = useState();
-  const pathName = usePathname();
 
   useEffect(() => {
     setmounted(true);
@@ -32,30 +30,7 @@ const Navbar = () => {
 
         <div className='space-x-8 hidden md:block'>
           {userData.navLinks.map((navLink) => (
-            <Link
-              key={navLink.title}
-              href={navLink.path}
-              className={`text-base  ${
-                pathName === navLink.path
-                  ? 'text-gray-800 font-bold dark:text-gray-400'
-                  : 'text-gray-600 dark:text-gray-200 font-normal '
-              }`}>
-              {navLink.title}
-              {pathName === navLink.path && (
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='16'
-                  height='16'
-                  fill='currentColor'
-                  className='bi bi-arrow-down inline-block h-3 w-3'
-                  viewBox='0 0 16 16'>
-                  <path
-                    fillRule='evenodd'
-                    d='M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z'
-                  />
-                </svg>
-              )}
-            </Link>
+            <NavLinks title={navLink.title} path={navLink.path} />
           ))}
         </div>
         <div className='space-x-4 flex flex-row items-center'>
